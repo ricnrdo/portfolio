693a12de031403c0322cfa97bb45ef9e3e816084
Ricardo Puma <ricnrdo@gmail.com>
2026-01-17 21:54:39 -0500

    Fixed broken navigation. Currently working on 'About' section.

--- src/app.tsx
+++ src/app.tsx
@@ -6,6 +6,7 @@ import backgroundImage from './assets/img/background.jpg';
 
 import CurvedText from './components/curvedText';
 import ArrowDownIcon from './components/downArrow';
+import cutout from './assets/img/cutout.png';
 
 createRoot(document.getElementById('header')!).render(
   <StrictMode>
@@ -24,6 +25,24 @@ createRoot(document.getElementById('main')!).render(
         <div className="guideButton">
           <h2>More About Me</h2>
           <ArrowDownIcon />
+        </div>
+      </section>
+      <section id="about">
+        <div className="text-box">
+        <p>
+          I’m a developer passionate about crafting accessible, pixel-perfect user interfaces that blend thoughtful design with robust engineering. My favorite work lies at the intersection of design and development, creating experiences that not only look great but are meticulously built for performance and usability.<br/><br/>
+
+          Currently, I'm a Senior Front-End Engineer at Klaviyo, specializing in accessibility. I contribute to the creation and maintenance of UI components that power Klaviyo’s frontend, ensuring our platform meets web accessibility standards and best practices to deliver an inclusive user experience.<br/><br/>
+
+          In the past, I've had the opportunity to develop software across a variety of settings — from advertising agencies and large corporations to start-ups and small digital product studios. Additionally, I also released a comprehensive video course a few years ago, guiding learners through building a web app with the Spotify API
+        </p>
+        <button className="glass">
+          Learn More
+        </button>
+        </div>
+        <h1 className="title">CS Student & Developer</h1>
+        <div className="cutout-container">
+          <img className="cutout" src={cutout} width={200}/>
         </div>
       </section>
     </>
